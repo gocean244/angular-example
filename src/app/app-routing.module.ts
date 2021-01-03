f598431fd7f81848af773e61e38119c4d0e1c5b4
tsuyoshi.uchida <tsuyoshi.uchida@khobho.co.jp>
2021-01-03 19:01:45 +0900

    feat(Create):Create画面追加

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CreateComponent } from './create/create/create.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'welcome', loadChildren: () => import('./welcome/welcome.module').then(m => m.WelcomeModule)},
+  {path: 'create', loadChildren: () => import('./create/create.module').then(m => m.CreateModule)},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
