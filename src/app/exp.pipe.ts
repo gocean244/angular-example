9ca9c769afe5cfcacbd697358d4cfa0c4fc14023
Gocean <bangalorejapan@gmail.com>
2021-02-13 19:36:54 +0900

    feat(exp-pipe):経験値表示

---
+++ src/app/exp.pipe.ts
@@ -0,0 +1,31 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Pet } from './interface/pet';
+
+const expTable = [
+  20,
+  40,
+  100,
+  250,
+  500,
+  1000,
+  1500,
+  4000,
+  10000,
+];
+
+@Pipe({
+  name: 'exp'
+})
+export class ExpPipe implements PipeTransform {
+
+  transform(pet: Pet, ...args: unknown[]): unknown {
+    const totalExp: number = pet.exp;
+    const level: number = pet.level;
+    const baseExp: number = expTable[level - 2] || 0;
+    const nextExp: number = expTable[level - 1] - baseExp;
+    const exp: number = totalExp - baseExp;
+
+    return exp + ' / ' + nextExp;
+  }
+
+}
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -6,10 +6,11 @@ import { HomeComponent } from './home/home.component';
 import { PetComponent } from './pet/pet.component';
 
 import {MatProgressBarModule} from '@angular/material/progress-bar';
+import { ExpPipe } from '../exp.pipe';
 
 
 @NgModule({
-  declarations: [HomeComponent, PetComponent],
+  declarations: [HomeComponent, PetComponent, ExpPipe],
   imports: [
     CommonModule,
     HomeRoutingModule,
